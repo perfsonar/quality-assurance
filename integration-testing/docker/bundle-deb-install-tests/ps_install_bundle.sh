43cee080bb9433beda13c76714fd959d6663c720
Antoine Delvaux <antoine.delvaux@gmail.com>
2021-02-16 20:04:25 -0500

    Refactoring of OS management in Dockerfile and in general testing script.  By default the script will now build a docker image for all Debian/Ubuntu OS we support and will try to install the bundles on each of them.

--- integration-testing/docker/bundle-deb-install-tests/ps_install_bundle.sh
+++ integration-testing/docker/bundle-deb-install-tests/ps_install_bundle.sh
@@ -3,42 +3,32 @@
 # Remount /tmp allowing exec
 mount /tmp -o remount,exec
 
-DEBIAN_FRONTEND=noninteractive
-echo "Set DEBIAN_FRONTEND: $DEBIAN_FRONTEND"
-
-apt-get -y install apt-utils
-
-echo "Executing 'apt-get -y update'"
-apt-get -y update
-
-apt-get install -y wget
-apt-get install -y gnupg
-
-cd /etc/apt/sources.list.d/
-wget -qO - http://downloads.perfsonar.net/debian/perfsonar-official.gpg.key | apt-key add -
-wget -qO - http://downloads.perfsonar.net/debian/perfsonar-snapshot.gpg.key | apt-key add -
-
+apt-get update
 
 BUNDLE=$1
-if [ -n "$2" ]; then
-    REPO=$2
-    if [ ! -z $REPO ]
-    then
-        echo "Installing repo: $REPO"
-        cd /etc/apt/sources.list.d/
-        wget http://downloads.perfsonar.net/debian/$REPO.list
-    else
-        echo "REPO not specified; using PRODUCTION";
-        cd /etc/apt/sources.list.d/
-        wget http://downloads.perfsonar.net/debian/perfsonar-release.list
-    fi
+apt-get install -y $BUNDLE
+if [ "$?" -ne "0" ]; then
+    echo -e "\nSomething went wrong during installation\n"
+    exit 1
 fi
 
-echo "Executing 'apt-get -y update'"
-apt-get -y update
+echo -e "\nInstallation of bundle $BUNDLE went fine!\n"
 
-CMD="/usr/bin/apt-get install -y $BUNDLE"
+if [[ $BUNDLE =~ perfsonar-(core|testpoint|toolkit) ]]; then
+    # Run pscheduler to see if all is fine
+    echo "We'll now try to run pscheduler…"
+    # Wait a bit so that pScheduler is ready
+    sleep 60
+    pscheduler troubleshoot
+    if [ "$?" -ne "0" ]; then
+        # Try a second time as pScheduler might be a bit picky
+        pscheduler troubleshoot
+        if [ "$?" -ne "0" ]; then
+            echo -e "\nSomething went wrong with pScheduler\n"
+            exit 1
+        fi
+    fi
 
-echo "Executing '$CMD'"
+    echo -e "\npScheduler seems to be running fine!\n"
+fi
 
-eval $CMD
--- integration-testing/docker/bundle-deb-install-tests/test_install_instructions.sh
+++ integration-testing/docker/bundle-deb-install-tests/test_install_instructions.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 #######
-# test_install_instructions.sh
+# test_install_instructions.sh [repo]
 # REQUIRES:
 #  - jq
 #  - single-sanity to be built on the same host, if it's enabled
@@ -12,14 +12,18 @@
 # the instructions are changed, this script will need to be updated to match
 # Can optionally specify a repo to use. By default, uses the production repo.
 # Debian repo Options:
-# - perfsonar-release
+# - perfsonar-release (PRODUCTION)
 # - perfsonar-patch-snapshot
 # - perfsonar-minor-snapshot
 # - perfsonar-patch-staging
+# - perfsonar-minor-staging
 #######
 
-# default repo
-# if blank/not specified, PRODUCTION is used
+# default OS, if blank/not specified, Debian 9 is used
+declare -a OSimages=("jrei/systemd-debian:9" "jrei/systemd-debian:10" "jrei/systemd-ubuntu:16.04" "jrei/systemd-ubuntu:18.04")
+#declare -a OSimages=("jrei/systemd-debian:9")
+
+# default repo, if blank/not specified, PRODUCTION is used
 REPO="perfsonar-release"
 
 # set repo if commandline argument is set
@@ -27,67 +31,67 @@ if [ -n "$1" ]; then
     REPO=$1
 fi
 
-echo "REPO: $REPO"
-
-# mj82 note:
-# Currently (as of 01/27/2021), the bundles that don't include
-# esmond work OK, but those that do have esmond error out
-# when installing
-
 # DECLARE WHICH BUNDLE(S) TO INSTALL
 
 #declare -a BUNDLES=("perfsonar-testpoint")
 #declare -a BUNDLES=("perfsonar-core")
 #declare -a BUNDLES=("perfsonar-toolkit")
-#declare -a BUNDLES=("perfsonar-tools" "perfsonar-core")
+#declare -a BUNDLES=("perfsonar-tools")
 #declare -a BUNDLES=("perfsonar-tools" "perfsonar-toolkit")
 declare -a BUNDLES=("perfsonar-tools" "perfsonar-testpoint" "perfsonar-core" "perfsonar-centralmanagement" "perfsonar-toolkit")
 
 TEXT_STATUS=""
 OUT=""
 docker-compose down
-docker-compose build --no-cache --force-rm debian_clean
-docker rm -f install-single-sanity
-for BUNDLE in ${BUNDLES[@]}; do
-    echo "BUILD BUNDLE $BUNDLE"
-    docker-compose up -d
-    LABEL="$BUNDLE-$REPO"
-    CONTAINER="deb-install-transient"
-    echo "CONTAINER: $CONTAINER"
-    docker-compose exec --privileged debian_clean /usr/bin/ps_install_bundle.sh "$BUNDLE" "$REPO"
-    STATUS=$?
-    echo "LABEL: $LABEL"
-    docker run --privileged --name install-single-sanity --network bundle_testing --rm single-sanity $CONTAINER $BUNDLE $REPO
-    SERVICE_STATUS=$?
-    #SERVICE_STATUS=1
-    OUT+="\n"
-    echo "LABEL: $LABEL"
-    echo -e "OUT:\n$OUT\n"
+# Loop on all OS we want to test
+for OSimage in ${OSimages[@]}; do
+    echo -e "OSimage: $OSimage - REPO: $REPO\n"
+    TEXT_STATUS+="OSimage: $OSimage - REPO: $REPO\n"
+    export OSimage REPO
+    # First we build our image
+    # TODO: should move to --no-cache when run on Jenkins or else?
+    docker-compose build --force-rm install_test
+    docker rm -f install-single-sanity
+    # Loop on all bundles we want to test
+    for BUNDLE in ${BUNDLES[@]}; do
+        docker-compose down
+        echo "TEST BUNDLE $BUNDLE"
+        docker-compose up -d
+        LABEL="$BUNDLE on $REPO"
+        CONTAINER="$OSimage_$REPO"
+        docker-compose exec --privileged install_test /usr/bin/ps_install_bundle.sh "$BUNDLE"
+        STATUS=$?
 
-    echo "$BUNDLE Tried to install; status: $STATUS"
-    if [ "$STATUS" -eq "0" ]; then
-        echo "$BUNDLE install SUCCEDED!"
-        TEXT_STATUS+="$BUNDLE install SUCCEEDED!\n"
-        #exit;
-    else
-        echo "$BUNDLE install FAILED!"
-        TEXT_STATUS+="$BUNDLE install FAILED!\n"
-        TEXT_STATUS+="QUITTING ... \n"
-#        exit
-    fi
-    if [ "$SERVICE_STATUS" -eq "0" ]; then
-        echo "$BUNDLE service checks RAN OK!"
-        TEXT_STATUS+="$BUNDLE service checks RAN OK!\n"
-        #exit;
-    else
-        echo "$BUNDLE service checks FAILED TO RUN!"
-        TEXT_STATUS+="$BUNDLE services FAILED TO RUN!\n"
-    fi
-    docker-compose down
+        echo "$BUNDLE Tried to install; status: $STATUS"
+        if [ "$STATUS" -eq "0" ]; then
+            echo "$BUNDLE install SUCCEDED!"
+            TEXT_STATUS+="$BUNDLE install SUCCEEDED!\n"
+        else
+            echo "$BUNDLE install FAILED!"
+            TEXT_STATUS+="$BUNDLE install FAILED!\n"
+            TEXT_STATUS+="QUITTING ... \n"
+            continue
+        fi
+        echo -e "\nLABEL: $LABEL\n"
+        docker run --privileged --name install-single-sanity --network bundle_testing --rm single-sanity install-test $BUNDLE $REPO
+        SERVICE_STATUS=$?
+        # TODO: try to capture output from run
+        OUT+="\n"
+        echo -e "OUT:\n$OUT\n"
 
-    echo -e $TEXT_STATUS
+        if [ "$SERVICE_STATUS" -eq "0" ]; then
+            echo "$BUNDLE service checks RAN OK!"
+            TEXT_STATUS+="$BUNDLE service checks RAN OK!\n"
+        else
+            echo "$BUNDLE service checks FAILED TO RUN!"
+            TEXT_STATUS+="$BUNDLE services FAILED TO RUN!\n"
+        fi
+
+        echo -e $TEXT_STATUS
+    done
 done
-    echo -e "OUT:\n$OUT\n"
-    echo -e $OUT | jq .
 
+echo -e "OUT:\n$OUT\n"
+echo -e $OUT | jq .
 echo -e $TEXT_STATUS
+
